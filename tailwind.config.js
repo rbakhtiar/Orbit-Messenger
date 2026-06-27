150db9bd45cf58dc72f4ac7eeef934e070fcec3a
rbakhtiar <RahimBakhtair1122@gmail.com>
2026-06-27 21:20:11 +0500

    Add Tailwind CSS configuration with custom colors

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        orbit: {
+          500: "#5b21b6"
+        },
+        vena: "#0ea5a4"
+      }
+    }
+  },
+  plugins: []
+};
